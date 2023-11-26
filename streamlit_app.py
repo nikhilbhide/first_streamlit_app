38188c246ce456a24fc70c8845ebd0f193e79ae3
Nikhil Bhide <nikhilbhide15@gmail.com>
2023-11-26 17:26:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
+import streamlit 
+
+streamlit.title("Here is the first app")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
